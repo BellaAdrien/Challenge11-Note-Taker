3a4e2f18566d3d52c6b02044664cdd137b73de72
BellaAdrien <Iadrien2020@fau.edu>
2023-08-30 20:47:16 -0400

    save11

--- Develop/public/assets/js/Main/routes/index.js
+++ Develop/public/assets/js/Main/routes/index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 
 // Import our modular routers for /tips and /feedback
-const tipsRouter = require('./tips');
-const feedbackRouter = require('./feedback');
-const diagnosticsRouter = require('./diagnostics');
+const notesRouter = require('./notes');
+// const feedbackRouter = require('./feedback');
+// const diagnosticsRouter = require('./diagnostics');
 
 const app = express();
 
